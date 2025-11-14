5976d03e14411bd44c655e6e3d678fee0a8cb079
mthiagoalves <m.thiago.ales@live.com>
2025-11-14 11:10:56 -0300

    feat(MetaAuthController): create new controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CampaignController;
 use App\Http\Controllers\ConnectionController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MetaAuthController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -23,6 +24,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/campaigns', [CampaignController::class, 'index'])
         ->name('campaigns.index');
+
+    Route::get('/auth/meta/redirect', [MetaAuthController::class, 'redirect'])
+        ->name('meta.redirect');
+
+    Route::get('/auth/meta/callback', [MetaAuthController::class, 'callback'])
+        ->name('meta.callback');
 });
 
 require __DIR__ . '/settings.php';
